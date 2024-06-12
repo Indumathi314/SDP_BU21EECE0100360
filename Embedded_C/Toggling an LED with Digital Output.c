1a01e97ecf2cf7490d6834c51ec5043dd80c46e8
Indumathi314 <indumathir314@gmail.com>
2024-06-12 14:48:51 +0530

    Update Toggling an LED with Digital Output.c

--- Embedded_C/Toggling an LED with Digital Output.c
+++ Embedded_C/Toggling an LED with Digital Output.c
@@ -1,22 +1,35 @@
-#include <avr/io.h>
-#include <util/delay.h>
+Objective:
+The objective of this program is to demonstrate how to control an LED using an AVR microcontroller. 
+The LED is turned on and off at regular intervals to create a blinking effect.
+Code:
+#include <avr/io.h>     // Include AVR I/O header file for I/O operations
+#include <util/delay.h> // Include utility delay functions header file
 
-// Initialize output pin
+// Function to initialize the output pin
 void IO_Init() {
     // Set PD3 as output (LED)
     DDRD |= (1 << PD3);
+    // DDRD: Data Direction Register for Port D
+    // (1 << PD3): Set PD3 as output
 }
 
+// Main function
 int main() {
     IO_Init(); // Call the function to initialize I/O
 
-    while (1) {
+    while (1) { // Infinite loop
         // Turn on the LED
         PORTD |= (1 << PD3);
+        // PORTD: Data Register for Port D
+        // (1 << PD3): Set PD3 (LED pin) to HIGH, turning on the LED
+
         _delay_ms(500); // Wait for 500 milliseconds
 
         // Turn off the LED
         PORTD &= ~(1 << PD3);
+        // PORTD: Data Register for Port D
+        // ~(1 << PD3): Clear PD3 (LED pin), turning off the LED
+
         _delay_ms(500); // Wait for 500 milliseconds
     }
 }
